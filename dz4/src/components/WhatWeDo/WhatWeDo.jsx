8cff896b0f8e002b1c988d3e1d22ddf2bd65474b
Oleksandr Tishchenko <oleks.tish@gmail.com>
2023-03-07 20:48:31 +0200

    add whatWeDo component

--- dz4/src/components/WhatWeDo/WhatWeDo.jsx
+++ dz4/src/components/WhatWeDo/WhatWeDo.jsx
@@ -1,21 +1,24 @@
 import { Col, Container, Row } from 'react-bootstrap';
-// import s from './WhatWeDo.module.scss';
+import s from './WhatWeDo.module.scss';
 import Card from '../Card/Card';
+import dot_img_1 from '../../assets/images/dot_1.png'
 
 const WhatWeDo = () => {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <h3>WHAT WE DO</h3>
-                    <h2>Our Service</h2>
-                </Col>
-            </Row>
-            <Row>
+        <div className={s.what_we_do}>
+            <Container>
+                <Row>
+                    <Col>
+                        <h3 className={s.what_we_do__subHeading}>WHAT WE DO</h3>
+                        <h2 className={s.what_we_do__heading}>Our Service</h2>
+                    </Col>
+                </Row>
                 <Card />
-            </Row>
-            <Row></Row>
-        </Container>
+                <Row>
+                    <img className={s.what_we_do__dot_img} src={dot_img_1} alt="" />
+                </Row>
+            </Container>
+        </div >
     );
 }
 
